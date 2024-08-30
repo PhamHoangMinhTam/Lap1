db03ad4a9c8f4ee6a54b98c1dfbc77f4c3dfe300
PhamHoangMinhTam <ptam0791@gmail.com>
2024-08-30 22:12:17 +0700

    Add files via upload

---
+++ bai13.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 16:10:01 2024
+
+@author: TAM PC
+"""
+a = int(input("Nhập ngày: "))
+b = int(input("Nhập tháng: "))
+c = int(input("Nhập năm: "))
+print(f"{a}/{b}/{c}:")
+a = int(input("Nhập ngày: "))
+b = int(input("Nhập tháng: "))
+c = int(input("Nhập năm: "))
+t = c%100 
+print(f"{a}/{b}/{t}:")
+a = int(input("Nhập ngày: "))
+b = int(input("Nhập tháng: "))
+c = int(input("Nhập năm: "))
+print(f"{c}/{b}/{a}:")
